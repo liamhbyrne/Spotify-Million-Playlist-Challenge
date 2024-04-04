e282608f6e4c2efdfe4c979ea75717c2e83b569e
Liam Byrne <lhb1g20@soton.ac.uk>
2024-04-04 13:06:17 +0100

    remove config.py from gitignore

---
+++ config.py
@@ -0,0 +1,5 @@
+# sqlite3 path. Database will be created if it does not exist.
+DB_ADDRESS = r"C:\Users\Liam\Documents\COMP6252-DLT\group_cw\data_storage\spotify.db"
+
+# Path to the dataset folder containing the JSON slices
+DATASET_PATH = r"C:\Users\Liam\Documents\COMP6252-DLT\group_cw/sample-data"
